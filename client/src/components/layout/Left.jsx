a5f10769deb619a0cc1b4e2b4d6c319ff269184b
Zeb <elizabeth.delk@colorado.edu>
2019-08-09 11:40:05 -0600

    convert Left from class to function react component for hooks

--- client/src/components/layout/Left.jsx
+++ client/src/components/layout/Left.jsx
@@ -33,34 +33,51 @@ const styleClosed = width => {
   }
 }
 
-export default class Left extends React.Component {
-  componentWillMount() {
-    this.setState({
-      open: this.props.open,
-    })
+const Left = (props) => {
+  // const [open, setOpen] = useState(props.open)
+
+  // render
+  const {content, open, visible} = props
+  const width = props.width ? props.width : defaultWidth
+
+  if (!visible) {
+    return null
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.open !== this.state.open) {
-      this.setState(prevState => {
-        return {
-          ...prevState,
-          open: nextProps.open,
-        }
-      })
-    }
-  }
-
-  render() {
-    const {content, open, visible} = this.props
-    const width = this.props.width ? this.props.width : defaultWidth
-
-    if (!visible) {
-      return null
-    }
-
-    return (
-      <div style={open ? styleOpen(width) : styleClosed(width)}>{content}</div>
-    )
-  }
+  return (
+    <div style={open ? styleOpen(width) : styleClosed(width)}>{content}</div>
+  )
 }
+export default Left
+
+// export default class Left extends React.Component {
+//   componentWillMount() {
+//     this.setState({
+//       open: this.props.open,
+//     })
+//   }
+//
+//   componentWillReceiveProps(nextProps) {
+//     if (nextProps.open !== this.state.open) {
+//       this.setState(prevState => {
+//         return {
+//           ...prevState,
+//           open: nextProps.open,
+//         }
+//       })
+//     }
+//   }
+//
+//   render() {
+//     const {content, open, visible} = this.props
+//     const width = this.props.width ? this.props.width : defaultWidth
+//
+//     if (!visible) {
+//       return null
+//     }
+//
+//     return (
+//       <div style={open ? styleOpen(width) : styleClosed(width)}>{content}</div>
+//     )
+//   }
+// }
